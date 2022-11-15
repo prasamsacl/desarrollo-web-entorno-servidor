9caff9df00ce79ee36d21dc36b647a7e48820aba
Prasamsa Castelao López <pcastelaol22@fpcoruna.afundacion.org>
2022-11-15 14:51:37 +0100

    Bases de datos

---
+++ source.sql
@@ -0,0 +1,113 @@
+-- MariaDB dump 10.19  Distrib 10.5.15-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mysitedb
+-- ------------------------------------------------------
+-- Server version	10.5.15-MariaDB-0+deb11u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tComentarios`
+--
+
+DROP TABLE IF EXISTS `tComentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tComentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(2000) DEFAULT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `peliculas_id` int(11) NOT NULL,
+  `fecha` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `peliculas_id` (`peliculas_id`),
+  CONSTRAINT `tComentarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `tUsuarios` (`id`),
+  CONSTRAINT `tComentarios_ibfk_2` FOREIGN KEY (`peliculas_id`) REFERENCES `tPeliculas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tComentarios`
+--
+
+LOCK TABLES `tComentarios` WRITE;
+/*!40000 ALTER TABLE `tComentarios` DISABLE KEYS */;
+INSERT INTO `tComentarios` VALUES (1,'Esta interesante.',1,1,'28/07/2021'),(2,'Me encantó esta pelicula.',2,2,'24/08/2022'),(3,'Recomendada, es muy graciosa.',3,3,'09/11/2022'),(4,'Tiene buena pinta,seria interesante verla..',4,4,'10/11/2022'),(5,'Con ganas de verla, demasiado miedo.',5,5,'20/05/2022');
+/*!40000 ALTER TABLE `tComentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tPeliculas`
+--
+
+DROP TABLE IF EXISTS `tPeliculas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tPeliculas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `url_image` varchar(200) DEFAULT NULL,
+  `genero` varchar(50) DEFAULT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tPeliculas`
+--
+
+LOCK TABLES `tPeliculas` WRITE;
+/*!40000 ALTER TABLE `tPeliculas` DISABLE KEYS */;
+INSERT INTO `tPeliculas` VALUES (1,'Raging Fire','RagingFire.png','accion','crimen'),(2,'Tadeo Jones 3: La tabla esmeralda','TadeoJones.png','aventura','animacion'),(3,'Robo Mundical','RoboMundial.png','comedia','drama'),(4,'Navidad de golpe','NavidadDeGolpe.png','Romance','Romance'),(5,'Men','Men.png','Terror','Terror');
+/*!40000 ALTER TABLE `tPeliculas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tUsuarios`
+--
+
+DROP TABLE IF EXISTS `tUsuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tUsuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `contraseña` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tUsuarios`
+--
+
+LOCK TABLES `tUsuarios` WRITE;
+/*!40000 ALTER TABLE `tUsuarios` DISABLE KEYS */;
+INSERT INTO `tUsuarios` VALUES (1,'Prasamsa','Castelao Loepz','pcastelo@gmail.com','12345'),(2,'Paula','Casanegra Casanegra','pcc21@gmail.com','123465'),(3,'Francisco','Fernandez Dominguez','fernandezDom21@gmail.com','123456789'),(4,'Luis','Aye Aye','luisaye12345@gmail.com','123bdc'),(5,'Carlota','Martinez Suarez','cmarsu998@gmail.com','123yhgd34');
+/*!40000 ALTER TABLE `tUsuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-15 14:44:23
